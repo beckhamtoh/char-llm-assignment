49ae0220db67f94350addc8a3fa5748acd279131
beckhamtoh <beckhamtoh@gmail.com>
2025-10-31 04:28:32 +0800

    edited models_lstm.py

--- models/models_lstm.py
+++ models/models_lstm.py
@@ -1,3 +1,4 @@
+# %%writefile models/models_lstm.py
 import jax
 import jax.numpy as jnp
 import flax.linen as nn
@@ -21,7 +22,11 @@ class LSTMStack(nn.Module):
         cells = tuple(nn.LSTMCell(features=H, name=f"lstm_{i}") for i in range(self.n_layers))
 
         if init_state is None:
-            states = tuple(cell.initialize_carry(jax.random.PRNGKey(0), (B,), size=H) for cell in cells)
+            # NOTE: size is keyword-only in this Flax version
+            states = tuple(
+                cell.initialize_carry(jax.random.PRNGKey(0), (B,), size=H)
+                for cell in cells
+            )
         else:
             states = init_state
 
